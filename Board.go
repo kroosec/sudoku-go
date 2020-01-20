eae333e771de1ac5ad0109a25a506bed8beb6275
Hani Benhabiles <kroosec@gmail.com>
2020-01-20 14:10:54 +0100

    Improve performance.
    Directly check if value exists, instead of returning list of the cell's
    peers.
    
    This brings the test time of a dumb solver against a hard problem from
    ~46s down to ~3s.

--- Board.go
+++ Board.go
@@ -22,11 +22,6 @@ var (
 
 type Board [numRows][numColumns]int
 
-type Square struct {
-	row    int
-	column int
-}
-
 func NewBoard(str string) (*Board, error) {
 	if str != "" && len(str) < numSquares {
 		return nil, ErrInvalidBoardString
@@ -82,39 +77,52 @@ func (b *Board) SetValue(row, column, value int) error {
 	return nil
 }
 
-func getPeers(row, column int) []Square {
-	var peers []Square
-	for i := 0; i < numColumns; i++ {
-		if i != column {
-			peers = append(peers, Square{row, i})
-		}
-		if i != row {
-			peers = append(peers, Square{i, column})
-		}
-	}
-	startRow := ((row) / 3) * 3
-	startColumn := ((column) / 3) * 3
-	for i := startRow; i < startRow+3; i++ {
-		for j := startColumn; j < startColumn+3; j++ {
-			if i != row && j != column {
-				peers = append(peers, Square{i, j})
+func (b *Board) valueInBox(row, column, value int) bool {
+	boxRow := (row / 3) * 3
+	boxColumn := (column / 3) * 3
+	for i := boxRow; i < boxRow+3; i++ {
+		for j := boxColumn; j < boxColumn+3; j++ {
+			if i == row && j == column {
+				continue
+			}
+			if b[i][j] == value {
+				return true
 			}
 		}
 	}
-	return peers
+	return false
+}
+
+func (b *Board) valueInColumn(row, column, value int) bool {
+	for i := 0; i < numRows; i++ {
+		if i == row {
+			continue
+		}
+		if b[i][column] == value {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *Board) valueInRow(row, column, value int) bool {
+	for i := 0; i < numColumns; i++ {
+		if i == column {
+			continue
+		}
+		if b[row][i] == value {
+			return true
+		}
+	}
+	return false
 }
 
 func (b *Board) valueExists(row, column int, value int) bool {
 	if value == EmptySquare {
 		return false
 	}
-	peers := getPeers(row, column)
-	for _, square := range peers {
-		if b[square.row][square.column] == value {
-			return true
-		}
-	}
-	return false
+
+	return b.valueInColumn(row, column, value) || b.valueInRow(row, column, value) || b.valueInBox(row, column, value)
 }
 
 func (b *Board) GetValue(row, column int) (int, error) {
